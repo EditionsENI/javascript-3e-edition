7b177da377789ca258b0d9174cfa6318bdc4c93c
juliepotier <jpotier@eni.fr>
2019-01-24 17:10:23 +0100

    Create app.module.ts

---
+++ ionic3-screen_orientation/src/app/app.module.ts
@@ -0,0 +1,37 @@
+// Import de classes (système)
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { ScreenOrientation } from "@ionic-native/screen-orientation";
+
+// Import de classes (métier)
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Décorateur NgModule (indiquant les propriétés de la classe AppModule)
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    ScreenOrientation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+
+// Exposition de la classe principale (AppModule)
+export class AppModule {}

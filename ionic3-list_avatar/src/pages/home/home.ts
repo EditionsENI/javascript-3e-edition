78a2e7ca747a75f7c286ed643b7a9f555f2f4b6e
juliepotier <jpotier@eni.fr>
2019-01-24 16:48:27 +0100

    Create home.ts

---
+++ ionic3-list_avatar/src/pages/home/home.ts
@@ -0,0 +1,13 @@
+// Import de classes (système)
+import { Component } from '@angular/core';
+
+// Décorateur @Component
+// Description de la page (composant Angular) :
+// - templateUrl = Page HTML associée au script TypeScript
+@Component({
+  templateUrl: 'home.html'
+})
+
+// Classe de la page HomePage
+export class HomePage {
+}

9817d822bdef210e693aa051c7254c3735d6adce
juliepotier <jpotier@eni.fr>
2019-01-24 16:42:21 +0100

    Create app.module.ts

---
+++ ionic3-clipboard/src/app/app.module.ts
@@ -0,0 +1,37 @@
+// Import de classes (système)
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Clipboard } from '@ionic-native/clipboard';
+
+// Import de classes (métier)
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Décorateur NgModule (indiquant les propriétés de la classe AppModule)
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Clipboard
+  ]
+})
+
+// Exposition de la classe principale (AppModule)
+export class AppModule {}
